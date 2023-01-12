bc57ba1318add8fbe22ca3a126b85843206ba127
Ben Wu <benjamin.x.wu@gmail.com>
2023-01-12 07:58:50 -0500

    Update sketch.js

--- CreativeCoding/TuningVariables/sketch.js
+++ CreativeCoding/TuningVariables/sketch.js
@@ -1,40 +1,24 @@
 //Create a program that enables us to customize the number of squares in a grid
-let gif;
-
-function preload (){
-  gif = loadImage('assets/FinalGifAnimation.gif');
-
-}
-
-
 function setup() { //runs once
-
-createCanvas(windowWidth, windowWidth); //creates a canvas 720px by 720px
-background(18, 138, 34); //background color
+  createCanvas(windowWidth, windowWidth);
+  fill(250,223,242);
+  strokeWeight(1);
 }
 
+function draw() { //loopty loop
 
 
+  var num = 15; //variable for the number of squares in the array
+  var sideLen = windowWidth / num; //variable for the side length of each square
 
-function draw() { //runs in a loop
+  for (var y = 0; y < windowWidth; y = y + sideLen) { //loop to create rows in the y-direction
 
-//fill ():
-strokeWeight (1);
+    for (var x = 0; x < windowWidth; x = x + sideLen) { //loop to create a row of squares in the x-direction
 
-var num = 10; //variable for the number of squares in the array
-var sideLen = windowWidth / num; //variable for the side length of each square
-
-translate (-100, -100); //translation creates a bleed effect
-
-for (var y = 0; y < 2 * windowWidth; y = y + sideLen) { //loop to create rows in the y-direction
-for (var x = 0; x < 2 * windowWidth; x = x + sideLen) { //loop to create a row of squares in the x-direction
-
-image (gif, x, y, windowWidth/num, windowWidth/num);
-
-}
-}
-}
-
-function windowResized () {
-  resizeCanvas (windowWidth, windowHeight);
+      quad(x, y,
+          x + sideLen, y,
+          x + sideLen, y + sideLen,
+          x, y + sideLen);
+    }
+  }
 }
