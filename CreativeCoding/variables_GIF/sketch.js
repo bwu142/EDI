d79b678e6ec060d4858371f770fc46331f3fab68
Ben Wu <benjamin.x.wu@gmail.com>
2023-01-12 08:16:57 -0500

    Update sketch.js

--- CreativeCoding/variables_GIF/sketch.js
+++ CreativeCoding/variables_GIF/sketch.js
@@ -9,16 +9,13 @@ function preload (){
 function setup() { //runs once
 
 createCanvas(windowWidth, windowWidth); //creates a canvas 720px by 720px
-background(18, 138, 34); //background color
+background(21, 140, 176); //background color
 }
 
 
 
 
 function draw() { //runs in a loop
-
-//fill ():
-strokeWeight (1);
 
 var num = 15; //variable for the number of squares in the array
 var sideLen = windowWidth / num; //variable for the side length of each square
