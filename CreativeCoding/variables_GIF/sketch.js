b70afbfaef1a5b09be77c78026b13f0c9159447b
Ben Wu <benjamin.x.wu@gmail.com>
2023-01-12 00:19:37 -0500

    Updating with smaller squares

--- CreativeCoding/variables_GIF/sketch.js
+++ CreativeCoding/variables_GIF/sketch.js
@@ -24,7 +24,7 @@ for (var y = 0; y < windowWidth; y = y + sideLen) { //loop to create rows in the
 
 for (var x = 0; x < windowWidth; x = x + sideLen) { //loop to create a row of squares in the x-direction
 
-image (gif, x, y, windowWidth, windowWidth);
+image (gif, x, y, windowWidth/num, windowWidth/num);
 
 }
 }
