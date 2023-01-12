d369042d78d1a4176befeeaf3df128f396b6e96e
Ben Wu <benjamin.x.wu@gmail.com>
2023-01-12 00:32:29 -0500

    code debug

--- CreativeCoding/variables_GIF/sketch.js
+++ CreativeCoding/variables_GIF/sketch.js
@@ -13,19 +13,29 @@ function setup() { //runs once
   fill(250,223,242);
   strokeWeight(1);
 }
+  createCanvas(windowWidth, windowWidth); //creates a canvas 720px by 720px
 
-function draw() { //loopty loop
 
 
-var num = 15; //variable for the number of squares in the array
+function draw() { //runs in a loop
+background(50, 145, 168); //background color
+//fill ():
+//strokeWeight (1);
+
+var num = 15; //variable for the number of squares in the array (30?)
 var sideLen = windowWidth / num; //variable for the side length of each square
 
-for (var y = 0; y < windowWidth; y = y + sideLen) { //loop to create rows in the y-direction
+translate (-100, -100); //translation creates a bleed effect
 
-for (var x = 0; x < windowWidth; x = x + sideLen) { //loop to create a row of squares in the x-direction
+for (var y = 0; y < 2 * windowWidth; y = y + sideLen) { //loop to create rows in the y-direction
+for (var x = 0; x < 2 * windowWidth; x = x + sideLen) { //loop to create a row of squares in the x-direction
 
 image (gif, x, y, windowWidth/num, windowWidth/num);
 
 }
 }
+}
+
+function windowResized () {
+  resizeCanvas (windowWidth, windowHeight);
 }
